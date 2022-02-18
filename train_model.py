3e7a7fec9377d65c07c2ee85e593457db0f82f9c
Raisul Kibria <raisul.inc@gmail.com>
2022-02-18 13:33:49 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -14,9 +14,9 @@ def log_metadata(epoch, logs):
         
 def mbtr_ds_generator(directory_MBTR, directory_SASA):
     with open(directory_MBTR) as f:
-        x = np.load(f)
+        x = np.load(f, encoding='utf-8'))
     with open(directory_SASA) as f:
-        y = np.load(f)
+        y = np.load(f, encoding='utf-8')
     return x, y
 
 
