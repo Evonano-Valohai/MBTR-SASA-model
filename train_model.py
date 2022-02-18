fd3f611960a0ead867b480808e100cb4453e6846
Raisul Kibria <raisul.inc@gmail.com>
2022-02-18 13:21:00 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -36,10 +36,10 @@ def main():
     )
     
     
-    input_path_train_MBTR = valohai.inputs('dataset_train').paths("*.csv")
-    input_path_train_SASA = valohai.inputs('dataset_train_SASA').paths("*.csv")
-    input_path_test_MBTR = valohai.inputs('dataset_test').paths("*.csv")
-    input_path_test_SASA = valohai.inputs('dataset_test_SASA').paths("*.csv")
+    input_path_train_MBTR = valohai.inputs('dataset_train').path()
+    input_path_train_SASA = valohai.inputs('dataset_train_SASA').path()
+    input_path_test_MBTR = valohai.inputs('dataset_test').path())
+    input_path_test_SASA = valohai.inputs('dataset_test_SASA').path()
     
     x_train, y_train =  mbtr_ds_generator(input_path_train_MBTR, input_path_train_SASA)
     x_test, y_test = mbtr_ds_generator(input_path_test_MBTR, input_path_test_SASA)
