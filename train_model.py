77cadb37e77ef7364a4e681f41f45dac1a06a089
Raisul Kibria <raisul.inc@gmail.com>
2022-02-18 08:21:41 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -5,7 +5,6 @@ import valohai
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, BatchNormalization
 from tensorflow.keras.utils import plot_model
